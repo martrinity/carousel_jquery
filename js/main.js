de493c9d5ed9e9a2a5bca384278876481ba19d43
“Martina <ma.covarrubias.a@gmail.com>
2017-07-13 10:40:55 -0400

    resolviendo estilo y resizando imagenes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
-// Activate Carousel
-$("#carousel-animal").carousel();
-
-// Enable Carousel Indicators
-$(".item").click(function(){
-    $("#carousel-animal").carousel(1);
-});
-
-// Enable Carousel Controls
-$(".left").click(function(){
-    $("#carousel-animal").carousel("prev");
+$(document).ready(function(){
+    // Activate Carousel
+    $(#carousel-animal).carousel();
+    
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#carousel-animal").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#carousel-animal").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#carousel-animal").carousel(2);
+    });
+    $(".item4").click(function(){
+        $("#carousel-animal").carousel(3);
+    });
+    
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#carousel-animal").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#carousel-animal").carousel("next");
+    });
 });
